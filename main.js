4de734306f2d039edc48c76edc78883bb622b028
Lauren Shugrue <ellacorvo@gmail.com>
2015-07-13 17:15:57 -0600

    created first form

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-$(document).on('ready', function() {
+// $(document).on('ready', function() {
 
-	var roundImg  = $('<img src="..." alt="..." class="img-circle img-responsive">');
-	var squareImg = $('<img src="..." alt="..." class="img-rounded img-responsive">');
+	//for futire functionality
+	// var roundImg  = $('<img src="..." alt="..." class="img-circle img-responsive">');
+	// var squareImg = $('<img src="..." alt="..." class="img-rounded img-responsive">');
 
 	var yarns = [];
 
@@ -20,8 +21,29 @@ $(document).on('ready', function() {
 		this.needlesize = needlesize;
 	}
 
-	
+	Yarn.prototype.create = function() {
+		var newYarn = $('<div class="newYarn margin10"></div>');
+		newYarn.text("Brand: " + this.brand + "Weight: "  + this.weight + " Yardage: " + this.yards + " Needle Size: " + this.needlesize + " Color: " + this.color + ". ");
+		return newYarn;
+	}
+
+	$('.yarn-form').on('submit', function() {
+		var newBrand = $("#brand").val();
+		var newWeight = $("#weight").val();
+		var newYards = $("#yards").val();
+		var newNeedles = $("#needles").val();
+		var newColor = $("#color").val();				
+		var newYarnEntry = new Yarn(newBrand, newWeight, newYards, newNeedles, newColor);
+		var photo = $('#yarnPic').val();
+
+		var yarnPic = $('<img src= "' + photo + ' "alt="..." class="img-circle img-responsive center-block">');
+
+		$('#stash-container').append(newYarnEntry.create().append(yarnPic));
+
+		return false;
+
+	});
 
   
-});
+// });
 
