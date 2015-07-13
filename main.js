9b98781bb0191afb3d14fe667ef18b0cc38abda2
Lauren Shugrue <ellacorvo@gmail.com>
2015-07-13 16:01:33 -0600

    added some CSS

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 $(document).on('ready', function() {
+
+	var roundImg  = $('<img src="..." alt="..." class="img-circle img-responsive">');
+	var squareImg = $('<img src="..." alt="..." class="img-rounded img-responsive">');
+
+	var yarns = [];
+
+	var Yarn = function(brand, weight, yards, needlesize, color) {
+		this.brand      = brand;
+		this.weight     = weight;
+		this.yards      = yards;
+		this.needlesize = needlesize;
+		this.color      = color;
+	}
+
+	var Project = function(name, weight, yards, needlesize) {
+		this.name       = name;
+		this.weight     = weight;
+		this.yards      = yards;
+		this.needlesize = needlesize;
+	}
+
+	
+
   
 });
 
